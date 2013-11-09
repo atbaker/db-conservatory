727db3f78304c7169b206d58b612e5439b4d2b01
Andrew T. Baker <andrew.tork.baker@gmail.com>
2013-11-09 16:53:48 -0500

    Updating EC2 hostname

--- spinner/views.py
+++ spinner/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 import requests
 
 def show_containers(req):
-    r = requests.get('http://ec2-23-22-67-178.compute-1.amazonaws.com/containers')
+    r = requests.get('http://ec2-54-237-81-117.compute-1.amazonaws.com/containers')
     return HttpResponse(r)
 
 def create_container(req):
-    r = requests.post('http://ec2-23-22-67-178.compute-1.amazonaws.com/containers')
+    r = requests.post('http://ec2-54-237-81-117.compute-1.amazonaws.com/containers')
     return HttpResponse(r)
