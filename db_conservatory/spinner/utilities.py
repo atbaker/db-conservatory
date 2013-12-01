15a627d616068b7a05c9ecdd52fd50ef65780693
Andrew T. Baker <andrew.tork.baker@gmail.com>
2013-11-30 20:21:19 -0500

    Removing utilities.py

--- db_conservatory/spinner/utilities.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from urlparse import urljoin
-
-import requests
-
-def get_all_containers():
-    r = requests.get(urljoin(settings.SPIN_DOCKER_ENDPOINT, 'containers'))
-    containers = r.json()
-    return containers
