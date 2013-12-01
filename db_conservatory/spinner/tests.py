b834b44e6f01db9fec6626e9aba1e25b56bd4edd
Andrew T. Baker <andrew.tork.baker@gmail.com>
2013-12-01 17:09:21 -0500

    Using mocking for tests of spin-docker

--- db_conservatory/spinner/tests.py
+++ db_conservatory/spinner/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 from django.test.client import Client
 from .models import Database, Container
+from .utils import get, post
+from requests.exceptions import RequestException
+
+import responses
 
 import pdb
 
@@ -16,6 +20,13 @@ class DatabaseTestCase(TestCase):
         return db
 
     def _create_database_and_container(self, session_key='test'):
+        responses.add(responses.POST, 'http://localhost:5000/v1/containers',
+            body=u'{"db_port": "49154", "id": "ef458b3613b3", \
+                "name": "/teal_lizard", "ssh_port": "49153", \
+                "status": "running", "uri": "/v1/containers/ef458b3613b3", \
+                "username": ""}',
+            status=201)
+
         db = self._create_database()
         container = db.create_container(session_key)
         return container
@@ -26,12 +37,30 @@ class DatabaseTestCase(TestCase):
         self.assertEqual(db.image, 'mysqltoday')
         self.assertEqual(db.ports, '3306,22')
 
+    @responses.activate
     def test_create_container(self):
         container = self._create_database_and_container()
 
         self.assertIsInstance(container, Container)
 
+    @responses.activate
     def test_create_container_request(self):
+        responses.add(responses.GET, 'http://localhost:5000/v1/containers',
+            body=u'[]')
+
+        responses.add(responses.POST, 'http://localhost:5000/v1/containers',
+            body=u'{"db_port": "49154", "id": "ef458b3613b3", \
+                "name": "/teal_lizard", "ssh_port": "49153", \
+                "status": "running", "uri": "/v1/containers/ef458b3613b3", \
+                "username": ""}',
+            status=201)        
+
+        responses.add(responses.GET, 'http://localhost:5000/v1/containers/ef458b3613b3',
+            body=u'{"db_port": "49154", "id": "ef458b3613b3", \
+                "name": "/teal_lizard", "ssh_port": "49153", \
+                "status": "running", "uri": "/v1/containers/ef458b3613b3", \
+                "username": ""}',)               
+
         db = self._create_database()
 
         self.c.get('/')
@@ -41,7 +70,17 @@ class DatabaseTestCase(TestCase):
 
         self.assertIn("Your new %s database is ready!" % db, resp.content)
 
+    @responses.activate
     def test_view_my_containers(self):
+        responses.add(responses.GET, 'http://localhost:5000/v1/containers',
+            body=u'[]')
+
+        responses.add(responses.GET, 'http://localhost:5000/v1/containers/ef458b3613b3',
+            body=u'{"db_port": "49154", "id": "ef458b3613b3", \
+                "name": "/teal_lizard", "ssh_port": "49153", \
+                "status": "running", "uri": "/v1/containers/ef458b3613b3", \
+                "username": ""}',)
+
         self.c.get('/')
         container = self._create_database_and_container(self.c.session.session_key)
 
@@ -50,5 +89,15 @@ class DatabaseTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertIn(str(container), resp.content)
 
-    def test_get_util(self):
-        container = self._create_database_and_container()
+    def test_bad_request_return_none(self):
+        self.assertEqual(get('continers'), None)
+
+    @responses.activate
+    def test_bad_json_return_none(self):
+        responses.add(responses.GET, 'http://localhost:5000/v1/containers',
+            body=u'"db_port": "49154", "id": "ef458b3613b3", \
+                "name": "/teal_lizard", "ssh_port": "49153", \
+                "status": "running", "uri": "/v1/containers/ef458b3613b3", \
+                "username": ""}',)        
+        
+        self.assertEqual(get('containers'), None)
