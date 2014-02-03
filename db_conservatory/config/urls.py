f749b83c27924abce2e206f468b5f4676b34fc74
Andrew T. Baker <andrew.tork.baker@gmail.com>
2014-02-03 00:34:04 -0500

    Fixing fixture. Disabling registration

--- db_conservatory/config/urls.py
+++ db_conservatory/config/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', DatabaseList.as_view(), name='home'),
-    url(r'^register$', UserCreate.as_view(), name='register'),
+    # url(r'^register$', UserCreate.as_view(), name='register'),
     url(r'^login$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'home'}, name='logout'),
     url(r'^databases/', include('spinner.urls')),
